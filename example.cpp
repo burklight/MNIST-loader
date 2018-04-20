f6425d9865eaeb4298d6c8f5c1e5588e71d5ae75
Borja Rodriguez Galvez <borjarg@kth.se>
2018-04-20 09:49:54 +0200

    Upload of the MNIST loader framework for c++

---
+++ example.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include "./mnist/readMnist.hpp"
+using namespace std;
+
+int main()
+{
+    vector<vector<vector<int> > > testImages = ReadTestImages();//  ReadImages("./mnist/t10k-images-idx3-ubyte");
+    vector<vector<vector<int> > > trainImages = ReadTrainImages(); //ReadImages("./mnist/train-images-idx3-ubyte");
+    vector<int> testLabels = readTestLabels(); //ReadLabels("./mnist/t10k-labels-idx1-ubyte");
+    vector<int> trainLabels = readTrainLabels(); //ReadLabels("./mnist/train-labels-idx1-ubyte");
+
+    cout << "******************************************************" << endl;
+    cout << "Training example of number: " << trainLabels[0] << endl;
+    cout << "******************************************************" << endl;
+    for (unsigned int y = 0; y < 28; y++){
+      for (unsigned int z = 0; z < 28; z++){
+        cout << trainImages[0][y][z] << " ";
+      }
+      cout << endl;
+    }
+    cout << "******************************************************" << endl;
+    cout << "Test example of number: " << testLabels[0] << endl;
+    cout << "******************************************************" << endl;
+    for (unsigned int y = 0; y < 28; y++){
+      for (unsigned int z = 0; z < 28; z++){
+        cout << testImages[0][y][z] << " ";
+      }
+      cout << endl;
+    }
+
+  return 0;
+}
